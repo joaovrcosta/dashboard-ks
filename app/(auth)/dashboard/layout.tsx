e9d95e321015e150f20a0b60ede06612bb6ba5d0
joaovrcosta <joaovr.costa@gmail.com>
2024-12-12 19:11:26 -0300

    feat: added login page

---
+++ app/(auth)/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import { Header } from "@/components/Header";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/signin/page.tsx
@@ -0,0 +1,8 @@
+export default function SignInPage() {
+  return (
+    <>
+      <h1>Sign in page</h1>
+      <p>Sign in form</p>
+    </>
+  );
+}
